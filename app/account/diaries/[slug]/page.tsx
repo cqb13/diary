df1867f4fde5a2fc4d688c8989372c422ba8229e
cqb13 <cqb13.main@gmail.com>
2023-10-11 16:40:34 -0400

    Added more info

--- app/account/diaries/[slug]/page.tsx
+++ app/account/diaries/[slug]/page.tsx
@@ -17,6 +17,7 @@ import { DiaryContent } from "@lib/types/diary";
 import Input from "@/components/general/input";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { set } from "firebase/database";
 
 export default function Diaries({ params }: { params: { slug: string } }) {
   const router = useRouter();
@@ -52,6 +53,8 @@ export default function Diaries({ params }: { params: { slug: string } }) {
 
   const [modal, setModal] = useState(false);
 
+  const [moreInfo, setMoreInfo] = useState(false);
+
   const [entryOrderReversed, setEntryOrderReversed] = useState(false);
 
   const [searchValue, setSearchValue] = useState("");
@@ -76,11 +79,12 @@ export default function Diaries({ params }: { params: { slug: string } }) {
         setCreatedAt(dateConverter(data.createdAt));
         setUpdatedAt(dateConverter(data.updatedAt));
         setEntryOrderReversed(data.entryOrderReversed);
+        const reversedOrder = data.entryOrderReversed;
         new Promise((resolve) => {
           resolve(
             sortDiaryEntriesByDate(
               data.diaryContent as any[],
-              entryOrderReversed,
+              reversedOrder as boolean,
             ),
           );
         }).then((sortedData) => {
@@ -324,9 +328,44 @@ export default function Diaries({ params }: { params: { slug: string } }) {
             ></textarea>
           </>
         )}
-        <section className="mt-5 flex gap-2 justify-between rounded-xl bg-light-background p-2">
-          <p>Created: {createdAt}</p>
-          <p>Last Update: {updatedAt}</p>
+        <section className="flex flex-col justify-center mt-5 rounded-xl bg-light-background p-2">
+          <section className="flex gap-2 justify-between">
+            <p>Created: {createdAt}</p>
+            <button
+              className="rounded-md hover:bg-opacity-50 transition-all"
+              onClick={() => setMoreInfo(!moreInfo)}
+            >
+              {moreInfo ? (
+                <svg
+                  fill="#E8EDDF"
+                  width="20px"
+                  height="20px"
+                  viewBox="-5 -7.5 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="xMinYMin"
+                >
+                  <path d="M7.071 2.828l-4.95 4.95A1 1 0 0 1 .707 6.364L6.364.707a1 1 0 0 1 1.414 0l5.657 5.657a1 1 0 0 1-1.414 1.414l-4.95-4.95z" />
+                </svg>
+              ) : (
+                <svg
+                  fill="#E8EDDF"
+                  width="20px"
+                  height="20px"
+                  viewBox="-5 -8 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="xMinYMin"
+                >
+                  <path d="M7.071 5.314l4.95-4.95a1 1 0 1 1 1.414 1.414L7.778 7.435a1 1 0 0 1-1.414 0L.707 1.778A1 1 0 1 1 2.121.364l4.95 4.95z" />
+                </svg>
+              )}
+            </button>
+            <p>Last Update: {updatedAt}</p>
+          </section>
+          {moreInfo ? (
+            <section>
+              <p>{`Entries: ${diaryEntries.length}`}</p>
+            </section>
+          ) : null}
         </section>
         <section className="mt-5 flex gap-2 justify-between">
           <Input
